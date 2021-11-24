867120accc38792ffadd30f021b5ff14405a5f4c
Ahmad Yusron <ahmadyusronh@gmail.com>
2021-11-24 10:05:50 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+//Get the button:
+mybutton = document.getElementById('myBtn');
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = 'block';
+  } else {
+    mybutton.style.display = 'none';
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+new Typed('#typed', {
+  strings: ['Developer', 'Designer'],
+  typeSpeed: 40,
+  delaySpeed: 90,
+  loop: true,
+});
+
+function myFunction() {
+  document.getElementById('myDropdown').classList.toggle('show');
+}
+
+window.onclick = function (e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById('myDropdown');
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+};
